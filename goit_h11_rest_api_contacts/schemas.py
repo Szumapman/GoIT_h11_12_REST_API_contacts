822aa04d2f088719dfd484666eaa17a55aba2131
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-16 01:52:57 +0100

    Add PhoneNumber validator and default settings to id + fix fields names

--- goit_h11_rest_api_contacts/schemas.py
+++ goit_h11_rest_api_contacts/schemas.py
@@ -1,15 +1,20 @@
 from pydantic import BaseModel, Field, EmailStr
+from pydantic_extra_types.phone_numbers import PhoneNumber
 from datetime import date
 
+
 class ContactIn(BaseModel):
-    firstname: str = Field(max_length=150)
-    lastname: str = Field(max_length=150)
-    email: EmailStr = Field(max_length=50, unique=True)
-    phone: str = Field(max_length=50, unique=True)
+    first_name: str = Field(max_length=150)
+    last_name: str = Field(max_length=150)
+    email: EmailStr = Field(max_length=150, unique=True)
+    phone: PhoneNumber = Field(max_length=50, unique=True)
     birth_date: date
-    additional_info: str | None = Field(max_length=1000)
+    additional_info: str | None = None
+
+    PhoneNumber.default_region_code = "PL"
+    PhoneNumber.phone_format = "E164"
   
   
 class ContactOut(ContactIn):
-    id: int
+    id: int = Field(default=1, ge=1)
     
