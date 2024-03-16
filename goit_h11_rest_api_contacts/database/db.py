c9d29fb78f324495ed0cac28ba4a6af5eaa5c54e
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-16 05:58:49 +0100

    Delete unnecessary dependency

--- goit_h11_rest_api_contacts/database/db.py
+++ goit_h11_rest_api_contacts/database/db.py
@@ -7,12 +7,3 @@ SQLALCHEMY_DATABASE_URL = (
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
