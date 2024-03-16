322820ac7172cea58169f5e5510da4e0624e9192
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-16 01:50:47 +0100

    Fix model field names

--- goit_h11_rest_api_contacts/database/models.py
+++ goit_h11_rest_api_contacts/database/models.py
@@ -1,18 +1,15 @@
-from sqlalchemy import Column, Integer, String, Boolean, func, Table
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Contact(Base):
     __tablename__ = "contacts"
-    id = Column(Integer, primary_key=True)
-    firstname = Column(String(150), nullable=False)
-    lastname = Column(String(150), nullable=False)
-    email = Column(String(50), nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(150), nullable=False)
+    last_name = Column(String(150), nullable=False)
+    email = Column(String(150), nullable=False, unique=True)
     phone = Column(String(50), nullable=False, unique=True)
-    birth_date = Column('birth_date', DateTime, nullable=False)
-    additional_info = Column(String(1000), nullable=True)
-
+    birth_date = Column(DateTime, nullable=False)
+    additional_info = Column(String(500))
