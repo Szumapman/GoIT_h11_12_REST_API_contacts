f98dc821b3932bfb8976086e340d0aa6a22ce751
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-16 06:03:37 +0100

    Delete code used to test the connection

--- main.py
+++ main.py
@@ -8,10 +8,5 @@ app = FastAPI()
 app.include_router(contacts.router, prefix="/api")
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World!"}
-
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
