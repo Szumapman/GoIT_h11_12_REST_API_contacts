27bf99becad8ada13704d17bb78a84ae8b681c6d
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-16 01:50:00 +0100

    Fix formatting

--- goit_h11_rest_api_contacts/database/db.py
+++ goit_h11_rest_api_contacts/database/db.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:567234@localhost:5432/contacts_db"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://postgres:567234@localhost:5432/contacts_db"
+)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 # Dependency
 def get_db():
