a32314c1bf87889309212291e7950371635e0cb1
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-17 12:15:35 +0100

    Delete unnecessary comment with old sql url

--- goit_h11_rest_api_contacts/database/db.py
+++ goit_h11_rest_api_contacts/database/db.py
@@ -6,9 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
-# SQLALCHEMY_DATABASE_URL = (
-#     "postgresql+psycopg2://postgres:567234@localhost:5432/contacts_db"
-# )
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
