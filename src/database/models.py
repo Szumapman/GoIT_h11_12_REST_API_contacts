db4622fab8918d5275980c55c875513cac8a7985
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-21 12:08:25 +0100

    Add user model + relation with contact

--- src/database/models.py
+++ src/database/models.py
@@ -1,15 +1,30 @@
-from sqlalchemy import Column, Integer, String, DateTime, JSON
+from sqlalchemy import Column, Integer, String, DateTime, JSON, ForeignKey, func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class Contact(Base):
     __tablename__ = "contacts"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     first_name = Column(String(150), nullable=False)
     last_name = Column(String(150), nullable=False)
     email = Column(String(150), nullable=False, unique=True)
     phone = Column(String(50), nullable=False, unique=True)
     birth_date = Column(DateTime, nullable=False)
     additional_info = Column(JSON, nullable=True)
+    user_id = Column(
+        "user_id", ForeignKey("users.id", ondelete="CASCADE"), default=None
+    )
+    user = relationship("User", backref="contacts")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(150), nullable=False, unique=True)
+    email = Column(String(150), nullable=False, unique=True)
+    password = Column(String(150), nullable=False)
+    created_at = Column("created_at", DateTime, default=func.now())
+    refresh_token = Column(String(255), nullable=True)
