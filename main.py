f817d60932c3494892a2a555545508dbf5fc7509
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-21 21:19:43 +0100

    Add auth router

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 
-from src.routes import contacts
+from src.routes import contacts, auth
 
 app = FastAPI()
 
+app.include_router(auth.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
 
 
