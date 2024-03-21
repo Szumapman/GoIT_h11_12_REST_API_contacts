78db6e94fef0489bb9c47c4361a6defd6e7ea61b
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-21 19:14:45 +0100

    Change  get_repository() to  get_contact_repository() and add get_user_repository()

--- src/database/dependencies.py
+++ src/database/dependencies.py
@@ -1,9 +1,16 @@
 from src.repository.abstract_repository import (
     AbstractContactsRepository,
+    AbstractUsersRepository,
 )
+
 from src.repository.contacts import PostgresContactRepository
+from src.repository.users import PostgresUserRepository
 from src.database.db import SessionLocal
 
 
-def get_repository() -> AbstractContactsRepository:
+def get_contact_repository() -> AbstractContactsRepository:
     return PostgresContactRepository(SessionLocal())
+
+
+def get_user_repository() -> AbstractUsersRepository:
+    return PostgresUserRepository(SessionLocal())
