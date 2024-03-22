b4bfad1205320d78c89b998684fe510092d1c858
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-22 14:40:35 +0100

    Add a user to the functions to manage only the contacts of a particular user

--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -1,7 +1,8 @@
 from typing import List
 from fastapi import APIRouter, Depends, status, Query, Path
 
-from src.schemas import ContactIn, ContactOut
+from src.auth import auth_service
+from src.schemas import ContactIn, ContactOut, UserOut
 from src.repository.abstract_repository import (
     AbstractContactsRepository,
 )
@@ -19,10 +20,11 @@ async def read_contacts(
     upcoming_birthdays: None | bool = Query(
         None, description="Get contacts with birthdays in the next 7 days"
     ),
+    current_user: UserOut = Depends(auth_service.get_current_user),
     contact_repo: AbstractContactsRepository = Depends(get_contact_repository),
 ) -> List[ContactOut]:
     contacts = await contact_repo.get_contacts(
-        search_name, search_email, upcoming_birthdays
+        search_name, search_email, upcoming_birthdays, current_user
     )
     return contacts
 
@@ -30,32 +32,36 @@ async def read_contacts(
 @router.get("/{contact_id}")
 async def read_contact(
     contact_id: int = Path(description="The ID of the contact to get", gt=0),
+    current_user: UserOut = Depends(auth_service.get_current_user),
     contact_repo: AbstractContactsRepository = Depends(get_contact_repository),
 ) -> ContactOut:
-    contact = await contact_repo.get_contact(contact_id)
+    contact = await contact_repo.get_contact(contact_id, current_user)
     return contact
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_contact(
     contact: ContactIn,
+    current_user: UserOut = Depends(auth_service.get_current_user),
     contact_repo: AbstractContactsRepository = Depends(get_contact_repository),
 ) -> ContactOut:
-    return await contact_repo.create_contact(contact)
+    return await contact_repo.create_contact(contact, current_user)
 
 
 @router.put("/{contact_id}")
 async def update_contact(
     contact_id: int,
     contact: ContactIn,
+    current_user: UserOut = Depends(auth_service.get_current_user),
     contact_repo: AbstractContactsRepository = Depends(get_contact_repository),
 ) -> ContactOut:
-    return await contact_repo.update_contact(contact_id, contact)
+    return await contact_repo.update_contact(contact_id, contact, current_user)
 
 
 @router.delete("/{contact_id}")
 async def delete_contact(
     contact_id: int,
+    current_user: UserOut = Depends(auth_service.get_current_user),
     contact_repo: AbstractContactsRepository = Depends(get_contact_repository),
 ) -> ContactOut:
-    return await contact_repo.delete_contact(contact_id)
+    return await contact_repo.delete_contact(contact_id, current_user)
