c36b174fba3e8851981e70cefde28c62a06c6a01
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-29 19:53:42 +0100

    Add fastapi_limiter

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import os
+from dotenv import load_dotenv
+
 import uvicorn
+import redis.asyncio as redis
 from fastapi import FastAPI
+from fastapi_limiter import FastAPILimiter
 
 from src.routes import contacts, auth
 
+load_dotenv()
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
+
+
+@app.on_event("startup")
+async def startup_event():
+    redis_base = await redis.Redis(
+        host=os.getenv("REDIS_HOST"), port=6379, db=0, encoding="utf-8", decode_responses=True
+    )
+    await FastAPILimiter.init(redis_base)
 
 
 if __name__ == "__main__":
