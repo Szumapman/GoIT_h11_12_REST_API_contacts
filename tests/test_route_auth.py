2d3193e861c4e69266bddaaec71be2f75b81f087
Paweł Szumański <pawel.szumanski@tuta.io>
2024-04-12 03:35:36 +0200

    add context manager for redis

--- tests/test_route_auth.py
+++ tests/test_route_auth.py
@@ -1,23 +1,30 @@
-from unittest.mock import MagicMock
+import pytest
+from unittest.mock import MagicMock, patch
+from fastapi.security import HTTPAuthorizationCredentials
 
+from src.services.auth import auth_service
 from src.database.models import User
 
 
 def test_signup_success(client, user, monkeypatch):
-    mock_send_email = MagicMock()
-    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
-    response = client.post("/api/auth/signup", json=user)
-    assert response.status_code == 201, response.text
-    data = response.json()
-    assert data["user"]["email"] == user.get("email")
-    assert data["detail"] == "User successfully created"
+    with patch.object(auth_service, "redis_base") as mock_redis:
+        mock_redis.get.return_value = None
+        mock_send_email = MagicMock()
+        monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
+        response = client.post("/api/auth/signup", json=user)
+        assert response.status_code == 201, response.text
+        data = response.json()
+        assert data["user"]["email"] == user.get("email")
+        assert data["detail"] == "User successfully created"
 
 
 def test_signup_failure_user_already_exists(client, user):
-    response = client.post("/api/auth/signup", json=user)
-    assert response.status_code == 409, response.text
-    data = response.json()
-    assert data["detail"] == f"User with email: {user.get('email')} already exists"
+    with patch.object(auth_service, "redis_base") as mock_redis:
+        mock_redis.get.return_value = None
+        response = client.post("/api/auth/signup", json=user)
+        assert response.status_code == 409, response.text
+        data = response.json()
+        assert data["detail"] == f"User with email: {user.get('email')} already exists"
 
 
 def test_login_user_not_confirmed(client, user):
@@ -63,5 +70,3 @@ def test_login_failure_wrong_email(client, user):
     assert response.status_code == 401, response.text
     data = response.json()
     assert data["detail"] == "Incorrect email or password"
-
-    
